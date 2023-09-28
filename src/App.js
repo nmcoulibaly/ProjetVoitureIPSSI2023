3a9fa3c05bb2745d50aa83e963bf2eab5c549197
Sonita-M <sonitamorency@gmail.com>
2023-09-28 13:01:25 +0200

    Ajout footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Menu from './core/components/layout/Menu';
+import Footer from './core/components/layout/Footer';
 
 function App() {
   return (
     <div className="App">
         <BrowserRouter>
             <Menu></Menu>
+            <Footer></Footer>
         </BrowserRouter>
     </div>
   );
