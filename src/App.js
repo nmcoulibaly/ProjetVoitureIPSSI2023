5c7afaf7f4e42fa4a14a7d24cf492d2b016c5209
Sonita-M <sonitamorency@gmail.com>
2023-10-01 22:25:50 +0200

    correction erreur merge

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
 import Menu from './core/components/layout/Menu';
-import MainRoutes from './core/routes/MainRoutes';
 import { UserContext } from './core/contexts/AuthContext';
 import { useState } from 'react';
 import Footer from './core/components/layout/Footer';
 import { BrowserRouter } from 'react-router-dom';
-=======
->>>>>>> 2ad13173802c2c7eceae49d9183d1c0819ad74d0
 import MainRoutes from './components/routes/MainRoutes';
-import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   const [user, setUser] = useState(JSON.parse(sessionStorage.getItem('USER')));
@@ -17,6 +13,7 @@ function App() {
     <div className="App">
         <BrowserRouter>
             <Menu></Menu>
+            <MainRoutes></MainRoutes>
             <Footer></Footer>
         </BrowserRouter>
     </div>
--- src/components/routes/MainRoutes.js
+++ src/components/routes/MainRoutes.js
@@ -3,11 +3,13 @@ import Home from "../../pages/Home";
 import VoituresList from "../../pages/voitures/VoituresList";
 import MarquesList from "../../pages/marques/MarquesList";
 import DetailMarque from "../../pages/marques/DetailMarque";
+import Login from "../../pages/authentication/Login";
 
 const MainRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/auth/login" element={<Login></Login>} />
             <Route path="/voituresList" caseSensitive element={<VoituresList />} />
             <Route path="/marquesList" caseSensitive element={<MarquesList />} />
             <Route path="marques/detail/:brandID" caseSensitive element={<DetailMarque />} />
